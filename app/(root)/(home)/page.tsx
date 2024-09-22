55d1494a03f9e71a6d837ad151dc7a049151acba
manuc108 <manu.v23csai@nst.rishihood.edu.in>
2024-09-22 19:34:06 +0530

    fixed time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,26 +1,37 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
-  const now = new Date();
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  useEffect(() => {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    const dateString = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(now);
+
+    setTime(timeString);
+    setDate(dateString);
+  }, []);
+
   return (
-      <section className='flex size-full flex-col gap-10 text-white'>
-        <div className='h-[303px] w-full rounded-[20px] bg-hero bg-cover'>
-          <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-            <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-              Upcoming Meeting at: 10:00 PM
-            </h2>
-            <div className="flex flex-col gap-2">
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <div className='h-[303px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: {time}
+          </h2>
+          <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
-          </div>
         </div>
-        <MeetingTypeList />
-      </section>
-  )
+      </div>
+      <MeetingTypeList />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
